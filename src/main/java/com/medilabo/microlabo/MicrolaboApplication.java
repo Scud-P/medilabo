12dceb5be5488a4662a9c0e396878cc11264087e
PScud <escudie.pierre@gmail.com>
2024-07-05 13:29:15 -0400

    Initial commit

---
+++ src/main/java/com/medilabo/microlabo/MicrolaboApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.microlabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrolaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrolaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilabo/microlabo/MicrolaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.microlabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrolaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
