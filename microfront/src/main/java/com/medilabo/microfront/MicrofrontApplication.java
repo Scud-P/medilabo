e10820cab274698528dfffe753516348273145eb
PScud <escudie.pierre@gmail.com>
2024-07-17 09:37:05 -0400

    Initial commit for monorepo

---
+++ microfront/src/main/java/com/medilabo/microfront/MicrofrontApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.microfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrofrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrofrontApplication.class, args);
+	}
+
+}
---
+++ microfront/src/test/java/com/medilabo/microfront/MicrofrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.microfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrofrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
